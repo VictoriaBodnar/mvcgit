a227b653c7385273c022952c10fc7e47dfbc1bff
VictoriaBodnar <bodnarvik@gmail.com>
2018-05-03 22:17:57 +0200

    from home mvc 2.1

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,9 @@
-<?php
+ <?php
 return array (
 
+//'news/77' => 'news/view', 
+//'news/15' => 'news/view', 
+'news/([0-9]+)' => 'news/view', 
 'news' => 'news/index', //actionIndex в NewsController
 'products' => 'product/list', //actionList в ProductController
 
--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,9 +4,17 @@ class NewsController
 	
 	public function actionIndex()
 	{
-		echo 'NewsController actionIndex';
+		//echo 'NewsController actionIndex';
+		echo 'Список новостей';
 		return true;
 	}
+	public function actionView()
+	{
+		//echo 'NewsController actionIndex';
+		echo 'Просмотр одной новости';
+		return true;
+	}
+	
 
 }
 
